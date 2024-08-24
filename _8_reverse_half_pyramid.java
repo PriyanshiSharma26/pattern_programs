c6a37c0955ecc34b75c137b69ea4827a6981bdec
PriyanshiSharma26 <priyanshisharma8220708@gmail.com>
2024-08-24 10:49:53 -0700

    Add files via upload

---
+++ _8_reverse_half_pyramid.java
@@ -0,0 +1,21 @@
+public class _8_reverse_half_pyramid {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5-i+1;j++)
+            {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
+        
+    
+    
+
+
+    
+    
+
