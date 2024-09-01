b40e5abf57ca1f91e2464ab63665df53274b2217
PriyanshiSharma26 <priyanshisharma8220708@gmail.com>
2024-09-01 10:16:18 -0700

    Add files via upload

---
+++ _20_checker_board_pattern.java
@@ -0,0 +1,17 @@
+ class CheckerboardPattern {
+    public static void main(String[] args) {
+        int size = 8; // Size of the checkerboard
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
