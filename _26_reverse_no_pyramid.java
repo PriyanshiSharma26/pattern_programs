7d2cd8159b5ab41e68bcb5a7ee7886b7715f23f9
PriyanshiSharma26 <priyanshisharma8220708@gmail.com>
2024-09-03 10:34:07 -0700

    Add files via upload

---
+++ _26_reverse_no_pyramid.java
@@ -0,0 +1,15 @@
+ class ReverseNumberPyramid {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = n; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
