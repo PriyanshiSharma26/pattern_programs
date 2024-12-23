95ac6fe09e8848a71313a8ad31b535d4e7052e48
PriyanshiSharma26 <priya612683@gmail.com>
2024-12-23 23:20:43 +0530

    Add files via upload

---
+++ _33_plus.java
@@ -0,0 +1,26 @@
+public class _33_plus {
+    public static void main(String[] args) {
+
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                if(j==3)
+                {
+                    System.out.print(i+" ");
+                }
+                else if(i==3){
+                    System.out.print(j+" ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+
+        }
+        
+    }
+    
+}
